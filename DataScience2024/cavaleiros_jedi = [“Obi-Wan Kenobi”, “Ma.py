f09a5e8ed6e0e6ae38e23760a6648116d424a3c5
JoãoVieira <joaopedrodesousavieira@gmail.com>
2024-09-29 22:42:37 -0300

    Sprint 3 finalizada

--- DataScience2024/cavaleiros_jedi = [“Obi-Wan Kenobi”, “Ma.py
+++
@@ -1,7 +0,0 @@
-cavaleiros_jedi = [“Obi-Wan Kenobi”, “Mace Windu”, “Mestre Yoda”, “Luke Skywalker”, “Anakin Skywalker”]
-print(cavaleiros_jedi[0]) #Exibe o primeiro elemento da lista
-print(cavaleiros_jedi[1]) #Exibe o segundo elemento da lista
-print(cavaleiros_jedi[-1]) #Exibe o último elemento da lista
-print(cavaleiros_jedi[1:3]) #Exibe os elementos que estão entre os índices 1 e 3, onde o 3 não está incluído
-print(cavaleiros_jedi[:3]) #Exibe os elementos que estão entre o início da lista e o índice 3, onde o 3 não está incluído
-print(cavaleiros_jedi[2:]) #Exibe os elementos que estão entre o índice 2 e o final da lista
